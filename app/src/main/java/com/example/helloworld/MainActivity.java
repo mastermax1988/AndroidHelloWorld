a774852f8a2b9d942ec5f56f56fa2237aa34b0d5
Max Huber <huber.maxi@gmail.com>
2021-01-26 11:52:58 +0100

    counting

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int nr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        nr=0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt = (TextView) findViewById(R.id.hw);
+        Button btn = (Button) findViewById(R.id.btnJump);
+        btn.setOnClickListener((e)->{
+            nr++;
+            txt.setText("Hello World! " + nr);
+        });
     }
 }
