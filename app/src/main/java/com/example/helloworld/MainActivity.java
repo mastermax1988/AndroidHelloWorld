1e0648317c22212ca29c1728f444ceb913b1b987
Max Huber <huber.maxi@gmail.com>
2021-01-30 20:02:31 +0100

    cam

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.helloworld;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,11 +39,35 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this,MyDraw.class);
             startActivity(intent);
         });
+        findViewById(R.id.btnCam).setOnClickListener((e)->{
+            Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                startActivityForResult(cam,1);
+            }catch (Exception ex)
+            {
+                System.out.println(ex.toString());
+            }
+        });
         if(savedInstanceState==null)
             return;
         nr = savedInstanceState.getInt("nr");
         txt.setText("Hello World! " + nr);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        ImageView view = new ImageView(this);
+        view.setImageBitmap(imageBitmap);
+        setContentView(view);
+        view.setOnClickListener((e)->
+        {
+            recreate();
+        });
+    }}
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
